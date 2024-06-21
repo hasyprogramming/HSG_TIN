b2bf26659c8f5f5ddaf6c8c02a40e4d4afefb0bc
hasyprogramming <hasymandela@gmail.com>
2024-06-21 13:53:21 +0700

    add Quan/2023/vong1/bai3

--- Quan/2023/vong1/bai3.cpp
+++ Quan/2023/vong1/bai3.cpp
@@ -22,9 +22,29 @@ ll comp(pair<ll, ll> a, pair<ll, ll> b){
   if (a.first == b.first) return a.second > b.second;
   return a.first > b.first;
 }
+ll x;
+vector<ll> a(maxn, 0);
 int main(){
   ios_base::sync_with_stdio(0);
   cin.tie(0);
   cout.tie(0);
+  //freopen("INP.txt", "r", stdin);
+  //freopen("OUT.txt", "w", stdout);
+  ll cnt = 0;
+  while (cin >> x){
+    a[x]+=1;
+    cnt++;
+  }
+  bool pr = false;
+  for(int i = 1; i <= cnt; i++){
+    if (a[i] == 0){
+      cout << i << " ";
+      pr = true;
+    }
+  }
+  if (pr == false){
+    cout << -1;
+  }
+  cout << endl;
   return 0;
 }
