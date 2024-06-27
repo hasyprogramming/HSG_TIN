d6c96f1b637e4a6ca1d63bb63823e18f5e967ad4
hasyprogramming <hasymandela@gmail.com>
2024-06-27 22:39:59 +0700

    quan/2023/vong2/bai4

--- Quan/2023/vong2/bai4.cpp
+++ Quan/2023/vong2/bai4.cpp
@@ -24,11 +24,49 @@ ll comp(pair<ll, ll> a, pair<ll, ll> b){
   if (a.first == b.first) return a.second > b.second;
   return a.first > b.first;
 }
-ll a, b, c;
+ll m, n, k, a[5002][5002], s[5002][5002];
 int main(){
   ios_base::sync_with_stdio(0);
   cin.tie(0);
   cout.tie(0);
   // freopen("INP.txt", "r", stdin);
   // freopen("OUT.txt", "w", stdout);
+  
+  
+  for(int i =0; i < 5002; i++){
+    for(int j= 0; j < 5002; j++){
+      a[i][j] = 0;
+      s[i][j] = 0;
+    }
+  }
+
+  
+  
+  
+  
+  cin >> m >> n >> k;
+  for(int i = 0; i < m; i++){
+    for(int j= 0; j < n; j++){
+      cin >> a[i][j];
+    }
+  }
+  for(int i = 1; i <= m; i++){
+    for(int j= 1; j <= n; j++){
+      s[i][j] = a[i-1][j-1] + s[i][j-1]+s[i-1][j]-s[i-1][j-1];
+    }
+  }
+  ll cnt = 0;
+  ll ans = 0;
+  while (cnt <= max(n, m)){
+    for(int i = 1; i < m-cnt; i++){
+      for(int j= 1; j < n-cnt; j++){
+        ll dif = s[i+cnt][j+cnt]-s[i-1][j+cnt]-s[i+cnt][j-1]+s[i-1][j-1];
+        if (dif <= k){
+          ans = max(ans, cnt);
+        }
+      }
+    }
+    cnt++;
+  }
+  cout << ans+1 << endl;
 }
